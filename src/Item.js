1d14e8f88eddec325063dff49daf23b85be316fa
jeromelimzw <jerome.lim.zw@gmail.com>
2019-03-01 07:59:12 +0800

    fix alignment

--- src/Item.js
+++ src/Item.js
@@ -12,12 +12,12 @@ const Item = ({
 }) => {
   return (
     <div class="br3 ba bg-near-white b--light-red w-20 ma0 shadow-5 bw3 ma2">
-      <div className="ba ph2 pb2 b--washed-red bw3 ma0">
-        <h3 className="f6 mb0 tc ttu">{category}</h3>
+      <div className="ba pr2 pb2 b--washed-red bw3 ma0">
+        <h3 className="f6 mb0 mt0 tc ttu">{category}</h3>
         <hr className="mt1 w-30 center" />
         <img
           src={imgUrl}
-          className="w-100 bg-light-gray shadow-5 br-100 ba bw2 b--moon-gray"
+          className="w-100 bg-light-gray shadow-5 br-100 ba bw2 b--moon-gray grow pointer"
           alt={name}
         />
 
@@ -28,8 +28,8 @@ const Item = ({
         <p class="f5 lh-copy dark-gray mt1">{description}</p>
         <hr className="mt0 w-80 center" />
         <div className="flex justify-between fw7">
-          <button className="w-30 f3 br2 bg-near-white pointer flex justify-around grow">
-            <strong>{price}</strong>
+          <button className="w-30 f3 br2 bg-near-white pointer flex justify-around shadow-5 grow ml2">
+            <strong className="self-center">{price}</strong>
             <img
               src="\img\PokéCoin.png"
               alt="pokecoin"
@@ -41,7 +41,7 @@ const Item = ({
             <i
               class={`${
                 isLiked ? "fas" : "far"
-              } fa-heart hover-red red pointer grow-large`}
+              } fa-heart hover-red red pointer grow-large ml2`}
             />
           </span>
         </div>
