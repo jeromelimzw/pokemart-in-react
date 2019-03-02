a41e110ce3bcd5edf09c8d3438da6eff81154aaa
jeromelimzw <jerome.lim.zw@gmail.com>
2019-03-02 23:27:03 +0800

    add opacity filter in category picker

--- src/CategoryButton.js
+++ src/CategoryButton.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const CategoryButton = ({ id, imgUrl, color, handleCategory }) => {
   return (
     <span>
       <img
-        className={`w-40 pa0 ma2 ba br3 bg-near-white b--${color} pointer shadow-5 bw2 grow`}
+        className={`w-40 pa0 ma2 ba br3 bg-near-white b--${color} pointer shadow-5 bw2 grow o-50`}
         src={imgUrl}
         alt="category"
         id={id}
